37a4f24de0f537b98f35fa727f324e84dfdb7848
rainnier <rainnier@gmail.com>
2015-01-24 01:21:00 +0800

    Change bureau depending on selected member

--- client/app/loanform/edit.js
+++ client/app/loanform/edit.js
@@ -6,13 +6,17 @@ angular.module('testYeomanApp')
     if(loanformId && loanformId.length > 0) {
         $http.get('/api/loanforms/' + loanformId).success(function(loanform) {
           $scope.loanform = loanform;
-
-                $scope.loanform.loan_date = new Date(loanform.loan_date);
+          $scope.loanform.loan_date = new Date(loanform.loan_date);
             
 
         });
     }
 
+    $scope.member_bureau = "";
+    $scope.change_member = function() {
+    	$scope.loanform.bureau = $scope.getMemberById($scope.loanform.member).bureau;
+    };
+    
     $scope.saveItem = function() {
         if(loanformId && loanformId.length > 0) {
             $http.put('/api/loanforms/' + loanformId, $scope.loanform).success(function(loanform) {
@@ -46,12 +50,26 @@ angular.module('testYeomanApp')
     $http.get('/api/members').success(function(members) {
       $scope.memberList = members;
     });
-
-        
+    
+    $scope.getMemberById = function(id) {
+        var results = jQuery.grep($scope.memberList, function( member, i ) {
+            return ( member._id === id );
+        });
+        return results[0];
+    }
 
     $http.get('/api/bureaus').success(function(bureaus) {
       $scope.bureauList = bureaus;
     });
+    
+    $scope.getBureauById = function(id) {
+        var results = jQuery.grep($scope.bureauList, function( bureau, i ) {
+            return ( bureau._id === id );
+        });
+        return results[0];
+    }
+    
+    
 
         
   }]);
